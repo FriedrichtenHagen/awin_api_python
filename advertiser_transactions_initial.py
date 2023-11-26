9d5bf96a7bcbe516c2befb85f1f5a84184351b4b
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-11-26 17:55:22 +0100

    Add larger time frames. Overlapping still needs to be checked.

--- advertiser_transactions_initial.py
+++ advertiser_transactions_initial.py
@@ -21,38 +21,6 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 ADVERTISER_ID = os.getenv("ADVERTISER_ID")
 
-
-def loop_api_calls():
-    total_start_date_str = '2021-01-01'
-    total_start_date = datetime.strptime(total_start_date_str, '%Y-%m-%d').date()
-    total_end_date = date.today()
-    total_date_difference = (total_end_date - total_start_date).days
-    number_of_necessary_api_calls = math.ceil(total_date_difference/31)
-    days_for_last_api_call = total_date_difference % 31
-    print(f'days in last api call: {days_for_last_api_call}')
-    print(f'Number of api calls: {number_of_necessary_api_calls}')
-
-    result_df = pd.DataFrame()
-    for i in range(number_of_necessary_api_calls):
-        if i % 20 == 0 and i != 0:
-            # pause every 20 API calls for 1 minute?
-            print('theoretical pause')
-        # last api call. Uses less than 31 days
-        if(days_for_last_api_call and i == number_of_necessary_api_calls-1):
-            start_date = total_start_date + (i-1)*timedelta(days=31)
-            end_date = start_date + timedelta(days=days_for_last_api_call)
-            
-        else:
-            start_date = total_start_date + i*timedelta(days=31)
-            end_date = start_date + timedelta(days=31)
-        print(start_date, end_date)
-
-        newest_dataframe = fetch_data(start_date, end_date)
-        result_df = result_df.append(newest_dataframe, ignore_index=True)
-    print(result_df)
-    
-loop_api_calls()
-
 # function to access awin api
 def fetch_data(start_date, end_date):
     # template url
@@ -60,7 +28,7 @@ def fetch_data(start_date, end_date):
     # Example:
     # https://api.awin.com/advertisers/1001/transactions/?startDate=2017-02-20T00%3A00%3A00&endDate=2017-02-21T01%3A59%3A59&timezone=UTC
 
-    url = f'https://api.awin.com/advertisers/{ADVERTISER_ID}/transactions/?startDate={start_date}T00%3A00%3A00&endDate={end_date}T01%3A59%3A59&timezone=UTC&dateType=transaction'
+    url = f'https://api.awin.com/advertisers/{ADVERTISER_ID}/transactions/?startDate={start_date}T00%3A00%3A00&endDate={end_date}T23%3A59%3A59&timezone=UTC&dateType=transaction'
 
     # To use your token for authentication, send it via the http headers. Please use "Authorization" as the key, and "Bearer <addYourTokenHere>" as the value.
     headers = {
@@ -81,3 +49,45 @@ def fetch_data(start_date, end_date):
         return awin_df
     else:
         print("Error:", response.status_code)
+
+def loop_api_calls():
+    total_start_date_str = '2021-01-01'
+    total_start_date = datetime.strptime(total_start_date_str, '%Y-%m-%d').date()
+    total_end_date = date.today()
+    total_date_difference = (total_end_date - total_start_date).days
+    number_of_necessary_api_calls = math.ceil(total_date_difference/32)
+    days_for_last_api_call = total_date_difference % 32
+    print(f'days in last api call: {days_for_last_api_call}')
+    print(f'Number of api calls: {number_of_necessary_api_calls}')
+
+    result_df = pd.DataFrame()
+    for i in range(number_of_necessary_api_calls):
+        if i % 20 == 0 and i != 0:
+            # pause every 20 API calls for 1 minute?
+            print('theoretical pause')
+        # last api call. Uses less than 31 days
+        if(days_for_last_api_call and i == number_of_necessary_api_calls-1):
+            start_date = total_start_date + (i)*timedelta(days=32)
+            end_date = start_date + timedelta(days=days_for_last_api_call)
+            
+        else:
+            start_date = total_start_date + i*timedelta(days=32)
+            end_date = start_date + timedelta(days=31)
+        print(start_date, end_date)
+
+
+        # is there overlap between the different time frames??
+        # set times to cover whole day
+        # adjust 
+
+
+        newest_dataframe = fetch_data(start_date, end_date)
+        
+        result_df = pd.concat([result_df, newest_dataframe], ignore_index=True)
+        print(f"Shape of result_df: {result_df.shape}")
+
+    print(result_df)
+    result_df.to_csv('output.csv', index=False)
+
+loop_api_calls()
+
